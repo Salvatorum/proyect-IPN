1657d955f8f31f8833dc68a61315930ae30b0faa
Salvatorum <tscanosa@gmail.com>
2024-11-19 00:25:46 -0300

    v1.3: Actualización y corrección de estilos basados en feedback

--- resources/js/filtering.js
+++ resources/js/filtering.js
@@ -9,11 +9,50 @@ $(document).ready(function() {
     });
   });
 
-
-/* Direccionamiento a Viewing al hacer click a una fila de producto o artículo*/
+/* Direccionamiento a Viewing al hacer click en el nombre del artículo o producto*/
 $(document).ready(function() {
-    $("tr").click(function() {
+    $(".tdLink").click(function() {
         window.location.href = '../../viewing/viewing.html';
     });
 });
 
+
+/* Código de paginación de la tabla de resultados */
+
+$(document).ready(function() {
+
+  const rowsPerPage = 20;
+  let currentPage = 1;
+  const $rows = $('#table-result tbody tr');
+  const numRows = $rows.length;
+  const numPages = Math.ceil(numRows / rowsPerPage);
+
+  function showPage (page) {
+    const start = (page - 1) * rowsPerPage;
+    const end = Math.min( start + rowsPerPage, numRows);
+
+    $rows.hide();
+    $rows.slice(start, end).show();
+  }
+
+  function updatePage () {
+    $("#pagination").empty(); 
+
+    for (let i = 1; i <= numPages; i++) { 
+      let link = $("<a>").text(i).attr("href", "#").addClass("linkPage");
+      if (i === currentPage) {
+        link.addClass("active"); 
+      }
+      link.click(function(e) { 
+        e.preventDefault();     
+        currentPage = i;       
+        showPage(currentPage); 
+        updatePage();    
+      });
+      $("#pagination").append(link).append(" "); 
+    }
+  }
+
+  showPage(currentPage);
+  updatePage();
+});
